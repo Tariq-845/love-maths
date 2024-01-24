59fb991b8428325378291833288895c237c50187
Tariq Ebden <tebden16@gmail.com>
2024-01-24 16:27:37 +0000

    display addition question to the user on first load

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,37 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * Main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // Generates random numbers between 1 and 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
 
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
+    // } else if (gameType === 'subtract') {
+    //     displaySubtractQuestion(num1, num2);
+    // } else if (gameType === 'multiply') {
+    //     displayMultiplyQuestion(num1, num2);
+    // } else {
+    //     displayDivisionQuestion(num1, num2);
+    // }
 }
 
 function checkAnswer() {
@@ -42,8 +58,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
